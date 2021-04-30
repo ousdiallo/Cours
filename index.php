6c108f1d588a5133e5f4231eb9da46536caf2950
Ousmane Diallo <info.ouzdiallo@gmail.com>
2021-04-30 15:37:53 +0000

    Premier Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simplon</title>
+</head>
+
+<body>
+    <h1>Lorem, ipsum dolor.</h1>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, ratione.</div>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Molestias alias, enim perferendis ipsum dolore quas non ipsa.
+        Expedita suscipit quia temporibus nisi ad, veniam exercitationem facilis vitae aliquid illum quas.</p>
+</body>
+
+</html>
